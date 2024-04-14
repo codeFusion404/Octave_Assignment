dd5cfeb39851dd90e8952ecd8bee48fe5d307f57
aDJi2003 <adjidharmawan87@gmail.com>
2024-04-14 21:18:15 +0700

    feat: update value for electric field in different layer

--- electric_Field.m
+++ electric_Field.m
@@ -31,11 +31,38 @@ Ex = 3;
 Ey = 4;
 Ez = 1;
 
+Ex_tulang = 0.7317;
+Ey_tulang = 4;
+Ez_tulang = 1;
+
+Ex_lemak = 0.8174;
+Ey_lemak = 4;
+Ez_lemak = 1;
+
+Ex_otot = 0.3476;
+Ey_otot = 4;
+Ez_otot = 1;
+
 [x, y, z] = meshgrid(-2:0.5:2, -2:0.5:2, -2:0.5:2);
 
 Ex_grid = Ex * ones(size(x));
 Ey_grid = Ey * ones(size(y));
 Ez_grid = Ez * ones(size(z));
+
+in_tulang = (x >= 0 & x <= 2) & (y >= 1 & y <= 2);
+Ex_grid(in_tulang) = Ex_tulang;
+Ey_grid(in_tulang) = Ey_tulang;
+Ez_grid(in_tulang) = Ez_tulang;
+
+in_lemak = (x >= 1 & x <= 2) & (y >= -1 & y <= 1);
+Ex_grid(in_lemak) = Ex_lemak;
+Ey_grid(in_lemak) = Ey_lemak;
+Ez_grid(in_lemak) = Ez_lemak;
+
+in_otot = (x >= 0 & x <= 1) & (y >= -1 & y <= 1);
+Ex_grid(in_otot) = Ex_otot;
+Ey_grid(in_otot) = Ey_otot;
+Ez_grid(in_otot) = Ez_otot;
 
 quiver3(x, y, z, Ex_grid, Ey_grid, Ez_grid, 'b');
 hold off;
